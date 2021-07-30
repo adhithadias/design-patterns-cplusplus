7b8f8fe479c7324e27030d780ae425ca580fdfdc
Adhhitha Dias <kadhitha@purdue.edu>
2021-07-30 15:42:23 -0400

    add copy constructor

--- prototype/main.cpp
+++ prototype/main.cpp
@@ -19,12 +19,19 @@ struct Address {
 // if we make the Address a pointer, then main method cannot simply change the address values
 // if did so, it would change the address values in the original object too.
 // Hence the prototype design pattern fails in this scenario.
+
+// we can resolve this by defining a copy constructor
 struct Contact {
     string name;
     Address* address;
 
     Contact (const string &name, Address *address)
         : name(name), address(address) {}
+
+    Contact (const Contact& other) 
+        : name {other.name}
+        , address { new Address{ other.address->street, other.address->city, other.address->suite } }
+    {}
 
     friend ostream &operator<<(ostream &os, const Contact &contact) {
         os << "name: " << contact.name << ", address: " << *contact.address;
@@ -35,7 +42,7 @@ struct Contact {
 int main() {
     Contact john{"John Doe", new Address{"123 East Dr", "London", 123}};
     // Contact jane{"Jane Smith", Address{"123 East Dr", "London", 103}};
-    Contact jane = john; // shallow copy
+    Contact jane {john}; // shallow copy
     jane.name = "Jane Smith";
     jane.address->suite = 103;
 
