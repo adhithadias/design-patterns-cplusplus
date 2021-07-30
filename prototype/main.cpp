530cd3197fa2a4e1e3e1e074a3a1bcb49b251e45
Adhhitha Dias <kadhitha@purdue.edu>
2021-07-30 15:36:06 -0400

    change to address pointer

--- prototype/main.cpp
+++ prototype/main.cpp
@@ -16,25 +16,28 @@ struct Address {
     }
 };
 
+// if we make the Address a pointer, then main method cannot simply change the address values
+// if did so, it would change the address values in the original object too.
+// Hence the prototype design pattern fails in this scenario.
 struct Contact {
     string name;
-    Address address;
+    Address* address;
 
-    Contact (const string &name, const Address &address)
+    Contact (const string &name, Address *address)
         : name(name), address(address) {}
 
     friend ostream &operator<<(ostream &os, const Contact &contact) {
-        os << "name: " << contact.name << ", address: " << contact.address;
+        os << "name: " << contact.name << ", address: " << *contact.address;
         return os;
     }
 };
 
 int main() {
-    Contact john{"John Doe", Address{"123 East Dr", "London", 123}};
+    Contact john{"John Doe", new Address{"123 East Dr", "London", 123}};
     // Contact jane{"Jane Smith", Address{"123 East Dr", "London", 103}};
-    Contact jane = john;
+    Contact jane = john; // shallow copy
     jane.name = "Jane Smith";
-    jane.address.suite = 103;
+    jane.address->suite = 103;
 
     cout << john << endl << jane << endl << john << endl;
 
