67518afa58f276014c5ef79b4c49bb1abd783066
Adhhitha Dias <kadhitha@purdue.edu>
2021-07-30 12:11:41 -0400

    move PointFactory class inside the Point class and make the attributes private

--- factories/example/example.cpp
+++ factories/example/example.cpp
@@ -20,9 +20,16 @@ we have to introduce enum for type differenciation
 3) Next we add a seperate class called Factory for Point creation
 In the Gand of 4, there is no Factory class, there is only factory methods and abstracts
 But we are creating a concrete Factory class (not abstract)
+
+4) We can move the PointFactory inside the Point class to keep the 
+open-close principle intact because we made everything public in Point class
+Now since PointFactory is inside the Point class, the private members of the Point class
+are accessible from inside the PointFactory class
+
+5) We can even make the PointFactory class private
 */
 class Point {
-public:
+
     Point (float x, float y) : x(x), y(y) {}
 
     // Point (float a, float b, PointType type = PointType::cartesian) {
@@ -38,26 +45,28 @@ public:
 
     float x, y;
 
+public:
     friend ostream &operator<<(ostream& os, Point &p) {
         os << "x: " << p.x << ", y: " << p.y;
         return os;
     }
 
-};
+    struct PointFactory {
+        PointFactory() {}
+        static Point NewCartesian(float x, float y) {
+            return {x, y};
+        }
 
-struct PointFactory {
-    static Point NewCartesian(float x, float y) {
-        return {x, y};
-    }
+        static Point NewPolar(float r, float theta) {
+            return { r*cos(theta), r*sin(theta) };
+        }
+    };
 
-    static Point NewPolar(float r, float theta) {
-        return { r*cos(theta), r*sin(theta) };
-    }
 };
 
 int main() {
 
-    auto p = PointFactory::NewPolar(5, M_PI_4);
+    auto p = Point::PointFactory::NewPolar(5, M_PI_4);
 
     cout << p << endl;
 
