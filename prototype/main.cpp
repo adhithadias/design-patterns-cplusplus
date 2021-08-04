fa915f8a580062838886862fbd850bbc2e6a64db
Adhhitha Dias <kadhitha@purdue.edu>
2021-08-04 11:38:07 -0400

    introduce factory method for prototype employee creation

--- prototype/main.cpp
+++ prototype/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
@@ -37,9 +38,27 @@ struct Contact {
         , address { new Address{*(other.address)} }
     {}
 
+    ~Contact() { delete address; }
+
     friend ostream &operator<<(ostream &os, const Contact &contact) {
         os << "name: " << contact.name << ", address: " << *contact.address;
         return os;
+    }
+};
+
+struct EmployeeFactory {
+    static unique_ptr<Contact> new_main_office_employee(const string& name, const int suite) {
+        static Contact p{ "", new Address {"123 East Dr", "London", 0} };
+        return new_employee(name, suite, p);
+    }
+
+private:
+    static unique_ptr<Contact> new_employee(const string& name, const int suite,
+    const Contact& prototype) {
+        auto result = make_unique<Contact>(prototype);
+        result->name = name;
+        result->address->suite = suite;
+        return result;
     }
 };
 
@@ -51,6 +70,9 @@ int main() {
     jane.address->suite = 103;
 
     cout << john << endl << jane << endl << john << endl;
+
+    auto adhitha = EmployeeFactory::new_main_office_employee("Adhitha", 100);
+    cout << *adhitha << endl;
 
     return 0;
 }
