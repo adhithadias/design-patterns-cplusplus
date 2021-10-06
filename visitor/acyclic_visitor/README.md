19aef5d1a6c873c157cce27b048896166add01d6
Adhhitha Dias <kadhitha@purdue.edu>
2021-10-06 18:59:11 -0400

    visitor: add acyclic visitor

---
+++ visitor/acyclic_visitor/README.md
@@ -0,0 +1,3 @@
+In addition to GANG OF 4 visitor there is a different kind of visitor called acyclic visitor.
+
+We overloaded the visit function in classic visitor. This is possible when we only have a class hierarchy.
--- visitor/acyclic_visitor/main.cpp
+++ visitor/acyclic_visitor/main.cpp
@@ -1,7 +1,120 @@
 
 
+#include <sstream>
+#include <string>
+#include <iostream>
+template <typename Visitable>
+struct Visitor
+{
+    virtual void visit(Visitable& obj) = 0;
+};
+
+struct VisitorBase 
+{
+    virtual ~VisitorBase() = default;
+};
+
+struct Expression 
+{
+    virtual ~Expression() = default;
+
+    // we want to make sure VisitorBase is of some type we expect
+    // we use a dynamic cast to make sure of it
+    virtual void accept(VisitorBase& obj) 
+    // now implementing the accept method in the class hierarchy is not a must
+    {
+        using EV = Visitor<Expression>;
+        if (auto ev = dynamic_cast<EV*>(&obj)) { // we verify the visitor base
+            ev->visit(*this);
+        }
+    }
+};
+
+struct DoubleExpression : Expression
+{
+    double value;
+
+    DoubleExpression(double value) : value(value) {}
+    
+    // we want to make sure VisitorBase is of some type we expect
+    // we use a dynamic cast to make sure of it
+    virtual void accept(VisitorBase& obj)
+    {
+        // DoubleExpressionVisitor
+        using DEV = Visitor<DoubleExpression>;
+        if (auto dev = dynamic_cast<DEV*>(&obj)) { // we verify the visitor base
+            dev->visit(*this);
+        }
+    }
+};
+
+struct AdditionExpression : Expression
+{
+    Expression *left, *right;
+
+    AdditionExpression(Expression* left, Expression* right) : left(left), right(right) {}
+
+    ~AdditionExpression()
+    {
+        delete left;
+        delete right;
+    }
+    
+    // we want to make sure VisitorBase is of some type we expect
+    // we use a dynamic cast to make sure of it
+    virtual void accept(VisitorBase& obj)
+    {
+        // DoubleExpressionVisitor
+        using AEV = Visitor<AdditionExpression>;
+        if (auto aev = dynamic_cast<AEV*>(&obj)) { // we verify the visitor base
+            aev->visit(*this);
+        }
+    }
+};
+
+// Visitor base has nothing in it. 
+// It is just a marker for visitors
+struct ExpressionPrinter : VisitorBase,
+    Visitor<DoubleExpression>,
+    Visitor<AdditionExpression>
+{
+    void visit(AdditionExpression &obj) override // overloads predicated by out inherits
+    {
+        oss << "(";
+        obj.left->accept(*this);
+        oss << "+";
+        obj.right->accept(*this);
+        oss << ")";
+
+    }
+
+    void visit(DoubleExpression &obj) override // overloads predicated by out inherits
+    {
+        oss << obj.value;
+    }
+    
+    std::string str()
+    {
+        return oss.str();
+    }
+
+private:
+    std::ostringstream oss;
+};
+
 int main(int ac, char* av[]) 
 {
+    AdditionExpression* e = new AdditionExpression{
+        new DoubleExpression{1},
+        new AdditionExpression{
+            new DoubleExpression{2},
+            new DoubleExpression{3}
+        }
+    };
+
+    ExpressionPrinter ep;
+    ep.visit(*e);
+    std::cout << ep.str() << std::endl;
 
     return 0;
 }
